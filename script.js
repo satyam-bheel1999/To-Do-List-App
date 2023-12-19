af1e1029d91ee7d3aee60be6195de995bf3cb414
satyam-bheel1999 <satymbheel1999@gmail.com>
2023-12-19 23:31:52 +0530

    add new features

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-const item = document.getElementById("content");
-const toDoBox = document.getElementById("to-do-list");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-item.addEventListener(
-    "keyup",
-    function(event){
-        if(event.key == "Enter"){
-            toDoBox = this.value;
-            this.value = "";
-        }
+function addTask(){
+    if(inputBox.value === ""){
+        alert("You must write something");
     }
-)
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
